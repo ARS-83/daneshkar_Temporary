057459340a4b64e37a77f8998c087cecb8fcc54d
ARS <lyrdasyd266@gmail.com>
2026-01-04 22:06:46 +0330

    add process and progress and jsonhandler

--- python_training/bookbudy/models/book.py
+++ python_training/bookbudy/models/book.py
@@ -12,8 +12,9 @@ class Book:
         self.date_added = date_added
         self.is_ended = False
         Book.counter += 1
+
     def __str__(self):
-        return "%s by %s" % (self.title, self.author,)
+        return "%s by %s . this genre %s and have a %s you %s "  % (self.title, self.author, self.genre, self.pages, f'{"end him " if self.is_ended else "dont end him"}')
 
     def __repr__(self):
         return f"Book(title='{self.title}', author='{self.author}', genre='{self.genre}', pages={self.pages}, date_added={self.date_added}, is_ended={self.is_ended})"
@@ -21,10 +22,15 @@ class Book:
     def __eq__(self, other):
         return self.title == other.title
 
-    def change_status(self, read_pages: int):
-        if self.pages == read_pages:
+    def change_status(self, read_pages: int) -> bool:
+        if self.pages <= read_pages:
             self.is_ended = True
             return self.is_ended
 
         return self.is_ended
 
+if __name__ == "__main__":
+    b = Book(title="ars",author="ars", genre="sdfsd", pages=1, date_added=datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    test = b.__dict__.get("title", "")
+
+    print(test)
